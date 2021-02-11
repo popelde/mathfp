c875baa3067a02c919004edb2b1929df34246b35
popelde <50044800+popelde@users.noreply.github.com>
2021-02-11 19:39:46 +0100

    feat: Abstract Term over variable name type

--- src/Term.hs
+++ src/Term.hs
@@ -1,40 +1,42 @@
 module Term where
 
 --
--- A term defined as usual
+-- A term defined as usual, over:
+--   a: a type representing numbers
+--   b: a type representing names (of variables, functions etc.)
 --
 
-data Term a = Const a
-            | Var String
-            | Neg (Term a)
-            | (Term a) :+ (Term a)
-            | (Term a) :- (Term a)
-            | (Term a) :* (Term a)
-            | (Term a) :/ (Term a)
-            | (Term a) :^ (Term a) deriving (Eq)
+data Term a b = Const a
+            | Var b
+            | Neg (Term a b)
+            | (Term a b) :+ (Term a b)
+            | (Term a b) :- (Term a b)
+            | (Term a b) :* (Term a b)
+            | (Term a b) :/ (Term a b)
+            | (Term a b) :^ (Term a b) deriving (Eq)
 
 
 --
 -- Print Term's prettily
 --
 
-instance Show a => Show (Term a) where
+instance (Show a, Show b) => Show (Term a b) where
   showsPrec p e0 =
     case e0 of
      Const n -> showParen (p > 10) $ showsPrec p n
-     Var   x -> showParen (p > 10) $ showString x
-     x :+ y -> showParen (p > 6) $ showsPrec 6 x . showString " + " . showsPrec 7 y
-     x :- y -> showParen (p > 6) $ showsPrec 6 x . showString " - " . showsPrec 7 y
-     x :* y -> showParen (p > 7) $ showsPrec 7 x . showString " * " . showsPrec 8 y
-     x :/ y -> showParen (p > 7) $ showsPrec 7 x . showString " / " . showsPrec 8 y
-     x :^ y -> showParen (p > 8) $ showsPrec 8 x . showString "^" . showsPrec 9 y
+     Var   x -> showParen (p > 10) $ showsPrec p x
+     x :+ y  -> showParen (p >  6) $ showsPrec 6 x . showString " + " . showsPrec 7 y
+     x :- y  -> showParen (p >  6) $ showsPrec 6 x . showString " - " . showsPrec 7 y
+     x :* y  -> showParen (p >  7) $ showsPrec 7 x . showString " * " . showsPrec 8 y
+     x :/ y  -> showParen (p >  7) $ showsPrec 7 x . showString " / " . showsPrec 8 y
+     x :^ y  -> showParen (p >  8) $ showsPrec 8 x . showString  "^"  . showsPrec 9 y
 
 
 --
 -- Does the variable x occur in a term (freely)?
 --
 
-freeFrom :: String -> Term a -> Bool
+freeFrom :: Eq b => b -> Term a b -> Bool
 freeFrom x (Const c) = True
 freeFrom x (Var  y)  = x /= y
 freeFrom x (Neg  t)  = freeFrom x t
@@ -48,14 +50,14 @@ freeFrom x (s :^ t)  = freeFrom x s && freeFrom x t
 -- Some numbers (Haskell peculiarity for arbitrary numerical type)
 --
 
-zero :: Num a => Term a
+zero :: Num a => Term a b
 zero =  Const (fromInteger 0)
 
-one  :: Num a => Term a
+one  :: Num a => Term a b
 one  =  Const (fromInteger 1)
 
-two  :: Num a => Term a
+two  :: Num a => Term a b
 two  =  Const (fromInteger 2)
 
-three  :: Num a => Term a
+three  :: Num a => Term a b
 three  =  Const (fromInteger 3)
