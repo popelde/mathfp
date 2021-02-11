7832170dbe059e8c22a6d01fd1d7670cb9d162e5
popelde <50044800+popelde@users.noreply.github.com>
2021-02-11 21:41:37 +0100

    feat: more simplification rules added

--- src/Simp.hs
+++ src/Simp.hs
@@ -6,23 +6,52 @@ import Term
 -- Rules for simplification
 --
 
-simplify (Neg (Neg s))  = s
-simplify ((Const c) :- (Const a)) = Const (c - a)
-simplify (Const c) = Const c
+
+simplify  (Const c)               = Const c
+simplify ((Const c) :+ (Const d)) = Const (c + d)
+simplify ((Const c) :- (Const d)) = Const (c - d)
+simplify ((Const c) :* (Const d)) = Const (c * d)
+simplify ((Const c) :/ (Const d)) = Const (c / d)
+
 simplify (Var y) = Var y
 
-simplify (s :+ t) = simplify s :+ simplify t
+simplify (Neg (Const c)) = Const (-c)
+simplify (Neg (Neg s))   = simplify s
+simplify (Neg (s :- t))  = t :- s
+
+simplify (s :+ Neg t) = simplify s :- simplify t
+simplify (Neg s :+ t) = simplify t :- simplify s
+simplify (s :+ t)     = simplify s :+ simplify t
+
+simplify (s :- Neg t) = simplify s :+ simplify t
 simplify (s :- t) = simplify s :- simplify t
+
+simplify ((Neg s) :* (Neg t)) = simplify s :* simplify t
 simplify (s :* t) = simplify s :* simplify t
+
+simplify ((Neg s) :/ (Neg t)) = simplify s :/ simplify t
 simplify (s :/ t) = simplify s :/ simplify t
+
 simplify (s :^ t)
-  | t == zero = one
-  | t == one  = s
-  | otherwise = simplify s :^ simplify t
+  | simp_t == zero = one
+  | simp_t == one  = s
+  | otherwise      = simplify s :^ simp_t
+  where
+    simp_t = simplify t
 -- ...
 
 
-simp :: (Eq a, Eq b, Num a) => Term a b -> Term a b
+simp :: (Eq a, Eq b, Fractional a) => Term a b -> Term a b
 simp = until (\x -> x == simplify x) simplify
 
-
+{-
+*Simp> simplify  (Neg (Const 3)) :* (Neg (Const 2))
+(-3.0) * -2.0
+*Simp> simplify ((Neg (Const 5)) :+ (Neg (Const 3)) :* (Neg (Const 2)))
+3.0 * 2.0 - 5.0
+*Simp> simplify $ simplify ((Neg (Const 5)) :+ (Neg (Const 3)) :* (Neg (Const 2)))
+6.0 - 5.0
+*Simp> simp ((Neg (Const 5)) :+ (Neg (Const 3)) :* (Neg (Const 2)))
+1.0
+*Simp> 
+-}
