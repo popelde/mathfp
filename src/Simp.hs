ffd6df48d031d9e4e2d2fa913f6bb67991e5f782
popelde <50044800+popelde@users.noreply.github.com>
2021-02-11 20:32:46 +0100

    chore: Apply hlint's hints

--- src/Simp.hs
+++ src/Simp.hs
@@ -8,20 +8,21 @@ import Term
 
 simplify (Neg (Neg s))  = s
 simplify ((Const c) :- (Const a)) = Const (c - a)
-simplify (Const c) = Const c 
+simplify (Const c) = Const c
 simplify (Var y) = Var y
 
-simplify (s :+ t) = (simplify s) :+ (simplify t)
-simplify (s :- t) = (simplify s) :- (simplify t)
-simplify (s :* t) = (simplify s) :* (simplify t)
-simplify (s :/ t) = (simplify s) :/ (simplify t)
-simplify (s :^ t) 
+simplify (s :+ t) = simplify s :+ simplify t
+simplify (s :- t) = simplify s :- simplify t
+simplify (s :* t) = simplify s :* simplify t
+simplify (s :/ t) = simplify s :/ simplify t
+simplify (s :^ t)
   | t == zero = one
   | t == one  = s
-  | otherwise = (simplify s) :^ (simplify t)
+  | otherwise = simplify s :^ simplify t
 -- ...
 
 
-simp t = (until (\x -> x == simplify x) simplify t)
+simp :: (Eq a, Eq b, Num a) => Term a b -> Term a b
+simp = until (\x -> x == simplify x) simplify
 
 
