081004b98ee7211c85e2ea017b8299cc817e4432
popelde <50044800+popelde@users.noreply.github.com>
2021-02-11 21:38:44 +0100

    fix(showsPrec): clause for 'Neg' added

--- src/Term.hs
+++ src/Term.hs
@@ -6,6 +6,13 @@ module Term where
 --   b: a type representing names (of variables, functions etc.)
 --
 
+
+infixl 6 :+
+infixl 6 :-
+infixl 7 :*
+infixl 7 :/
+infixl 8 :^
+
 data Term a b = Const a
             | Var b
             | Neg (Term a b)
@@ -25,6 +32,7 @@ instance (Show a, Show b) => Show (Term a b) where
     case e0 of
      Const n -> showParen (p > 10) $ showsPrec p n
      Var   x -> showParen (p > 10) $ showsPrec p x
+     Neg   x -> showParen (p > 10) $ showString "-" . showsPrec p x
      x :+ y  -> showParen (p >  6) $ showsPrec 6 x . showString " + " . showsPrec 7 y
      x :- y  -> showParen (p >  6) $ showsPrec 6 x . showString " - " . showsPrec 7 y
      x :* y  -> showParen (p >  7) $ showsPrec 7 x . showString " * " . showsPrec 8 y
@@ -36,7 +44,7 @@ instance (Show a, Show b) => Show (Term a b) where
 -- Does the variable x occur in a term (freely)?
 --
 
-freeFrom :: Eq b => b -> Term a b -> Bool
+--freeFrom :: Eq b => b -> Term a b -> Bool
 freeFrom x (Const c) = True
 freeFrom x (Var  y)  = x /= y
 freeFrom x (Neg  t)  = freeFrom x t
@@ -51,13 +59,13 @@ freeFrom x (s :^ t)  = freeFrom x s && freeFrom x t
 --
 
 zero :: Num a => Term a b
-zero =  Const (fromInteger 0)
+zero =  Const 0
 
 one  :: Num a => Term a b
-one  =  Const (fromInteger 1)
+one  =  Const 1
 
 two  :: Num a => Term a b
-two  =  Const (fromInteger 2)
+two  =  Const 2
 
 three  :: Num a => Term a b
-three  =  Const (fromInteger 3)
+three  =  Const 3
